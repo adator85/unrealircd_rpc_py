1f445bcc3438a26e6901991bfe8d2c3cccb21e4a
adator <85586985+adator85@users.noreply.github.com>
2024-09-07 01:14:47 +0200

    Include Server_ban_Exception

--- unrealircd_rpc_py/Rpc.py
+++ unrealircd_rpc_py/Rpc.py
@@ -6,6 +6,7 @@ from unrealircd_rpc_py.Whowas import Whowas
 from unrealircd_rpc_py.Server import Server
 from unrealircd_rpc_py.Channel import Channel
 from unrealircd_rpc_py.Server_ban import Server_ban
+from unrealircd_rpc_py.Server_ban_exeption import Server_ban_exception
 
 class Rpc:
 
@@ -31,9 +32,12 @@ class Rpc:
 
         # Create Server Instance
         self.Server = Server(self.Connection)
-        
+
         # Create Server_ban Instance
         self.Server_ban = Server_ban(self.Connection)
+
+        # Create Server_ban_exception Instance
+        self.Server_ban_exception = Server_ban_exception(self.Connection)
 
         # Create Channel Instance
         self.Channel = Channel(self.Connection)
