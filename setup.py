77823f23884d5394ead1c2ae0980f4f066f328e4
adator <85586985+adator85@users.noreply.github.com>
2024-09-22 23:04:29 +0200

    V0.1.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='unrealircd_rpc_py',
-    version='0.1.2',
+    version='0.1.3',
     packages=find_packages(),
     install_requires=[
         "requests>=2.25.1"
--- unrealircd_rpc_py/Live.py
+++ unrealircd_rpc_py/Live.py
@@ -18,6 +18,8 @@ class Live:
         self.Logs: logging
         self.__init_log_system()
 
+        self.Error = self.ErrorModel(0, '')
+
         if not self.__check_unix_socket_file(path_to_socket_file=path_to_socket_file):
             self.Logs.critical(f'The socket file is not available, please check the full path of your socket file')
             self.Error.code = -1
@@ -36,8 +38,6 @@ class Live:
 
         # Option 2 with Namespaces
         self.json_response_np: SimpleNamespace
-
-        self.Error = self.ErrorModel(0, '')
 
     def __check_unix_socket_file(self, path_to_socket_file: str) -> bool:
         """Check provided full path to socket file if it exist
