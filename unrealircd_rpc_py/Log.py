07f62cb20c69eb3ff0260c4f7d00bd90583ab3ab
adator <85586985+adator85@users.noreply.github.com>
2024-09-27 01:16:02 +0200

    V0.1.6

--- unrealircd_rpc_py/Log.py
+++ unrealircd_rpc_py/Log.py
@@ -1,26 +1,12 @@
 from types import SimpleNamespace
-from typing import Union
-from dataclasses import dataclass
+from typing import Union, Literal
+from time import time
 from unrealircd_rpc_py.Connection import Connection
+# import Definition
 
 class Log:
 
-    @dataclass
-    class ModelLog:
-        type: str
-        type_string: str
-        set_by: str
-        set_at: str
-        expire_at: str
-        set_at_string: str
-        expire_at_string: str
-        duration_string: str
-        set_at_delta: int
-        set_in_config: bool
-        name: str
-        reason: str
-
-    DB_NAME_BANS: list[ModelLog] = []
+    # DB_LOG_CONNECT: list[Definition.LogConnect]
 
     def __init__(self, Connection: Connection) -> None:
 
@@ -68,80 +54,43 @@ class Log:
         except Exception as err:
             self.Logs.error(f'General error: {err}')
 
-    def subscribe(self, sources: list) -> bool:
-        """Subscribe to one or more log sources. Any previous subscriptions are overwritten (lost).
+    def send(self, msg: str, level: Literal['debug','info','warn','error','fatal'], subsystem: str, event_id: str, timestamp: float = time()) -> bool:
+        """ Send a log message / server notice.
+
+        Requires UnrealIRCd 6.1.8 or later
 
         Args:
-            sources (list): an array of log sources. See log block sources. 
-            For example ["!debug","all"] would give you all log messages except for debug messages. And ["connect"] would give only client connects/disconnects.
+            msg (str): The message you want to send to the server
+            level (Literal[&#39;debug&#39;,&#39;info&#39;,&#39;warn&#39;,&#39;error&#39;,&#39;fatal&#39;]): You can pick the level in the list
+            subsystem (str): You can put anything you want as a subsystem
+            event_id (str): You can put anything you want as a event_id
+            timestamp (float, optional): The current timestamp. Defaults to time().
 
         Returns:
-            bool: True if success
+            bool: True if is okey, else if there is an error
         """
+        # waiting for the documentation.
         try:
             response = self.Connection.query(
-                method='log.subscribe', 
-                param={"sources": sources}
-                )
+                    method='log.send',
+                    param={"msg": msg, "level": level, "subsystem": subsystem, "event_id": event_id, "timestamp": timestamp}
+                    )
 
             self.response_raw = response
             self.response_np = self.Connection.json_response_np
 
-            if response is None:
-                return False
-
             if 'error' in response:
+                self.Logs.error(response["error"])
                 self.Connection.set_error(response)
                 return False
 
-            if 'result' in response:
-                if response['result']:
-                    self.Logs.debug(response)
-                    return True
-                else:
-                    self.Logs.debug(response)
-                    return False
+            self.Logs.debug(response)
 
             return True
 
         except KeyError as ke:
             self.Logs.error(f'KeyError: {ke}')
+            return False
         except Exception as err:
             self.Logs.error(f'General error: {err}')
-
-    def unsubscribe(self) -> bool:
-        """Unsubscribe from all log events.
-        After this UnrealIRCd will stop streaming log events to you.
-
-        Returns:
-            bool: Always returns true
-        """
-        try:
-            response = self.Connection.query(
-                method='log.unsubscribe'
-                )
-
-            self.response_raw = response
-            self.response_np = self.Connection.json_response_np
-
-            if response is None:
-                return False
-
-            if 'error' in response:
-                self.Connection.set_error(response)
-                return False
-
-            if 'result' in response:
-                if response['result']:
-                    self.Logs.debug(response)
-                    return True
-                else:
-                    self.Logs.debug(response)
-                    return False
-
-            return True
-
-        except KeyError as ke:
-            self.Logs.error(f'KeyError: {ke}')
-        except Exception as err:
-            self.Logs.error(f'General error: {err}')
+            return False
