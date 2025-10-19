47845d5e554e45640b8793543fba4b0ce4c228c4
adator <85586985+adator85@users.noreply.github.com>
2025-10-19 21:37:47 +0200

    Update version app

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='unrealircd_rpc_py',
-    version='3.0.1',
+    version='3.0.2',
     packages=find_packages(where='.', exclude=["tests/*","test_*"]),
     install_requires=[
         "requests>=2.25.1",
