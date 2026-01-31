e745ca98171a05a27322b4f02ec5206fda3d79cd
adator <85586985+adator85@users.noreply.github.com>
2026-01-31 01:13:59 +0100

    Version update. Moving new features connthrottle & security group to the minimum ircd version of 6.2.3!

--- unrealircd_rpc_py/connections/sync/__init__.py
+++ unrealircd_rpc_py/connections/sync/__init__.py
@@ -2,7 +2,7 @@
 __version_required__: dict[str, tuple[int, int, int]] = {
     'log': (6, 1, 8),
     'message': (6, 2, 2),
-    'connthrottle': (6, 2, 2),
-    'security_group': (6, 2, 2)
+    'connthrottle': (6, 2, 3),
+    'security_group': (6, 2, 3)
 }
 """The minimum expected version by object"""
